5ecbf9d21d0dbe3f63aef06a01759c1b8ae34212
Ilya-tcv <chernychovilya@yahoo.fr>
2020-07-16 10:03:13 +0200

    Fix about cards to 3

--- resources/views/template/about.blade.php
+++ resources/views/template/about.blade.php
@@ -1,23 +1,34 @@
 	<!-- About section -->
 	<div class="about-section">
-		<div class="overlay"></div>
-		<!-- card section -->
-		<div class="card-section">
-			<div class="container">
-				<div class="row">
-					<!-- single card -->
-					@foreach ($about as $item)
-					<div class="col-md-4 col-sm-6">
-						<div class="lab-card">
-							<div class="icon">
-								<i class=" {{$item -> icon}} "></i>
-							</div>
-							<h2> {{$item -> title}} </h2>
-							<p> {{$item -> text}} </p>
-						</div>
+	    <div class="overlay"></div>
+	    <!-- card section -->
+	    <div class="card-section">
+	        <div class="container">
+	            <div class="row">
+				  {{-- SET INCREMENTATION --}}
+	                @php
+	                    $i = 0;
+	                @endphp
+
+				{{-- LOOP --}}
+	                @foreach($service as $item)
+	                    <!-- single card -->
+	                    <div class="col-md-4 col-sm-6">
+	                        <div class="lab-card">
+	                            <div class="icon">
+	                                <i class=" {{ $item -> icon }} "></i>
+	                            </div>
+	                            <h2> {{ $item -> title }} </h2>
+	                            <p> {{ $item -> desc }} </p>
+	                        </div>
 					</div>
-					@endforeach
-				</div>
-			</div>
-		</div>
-		<!-- card section end-->
+					
+					{{-- SET WHEN TO BREAK --}}
+	                    @php
+	                        if (++$i == 3) break;
+	                    @endphp
+	                @endforeach
+	            </div>
+	        </div>
+	    </div>
+	    <!-- card section end-->
